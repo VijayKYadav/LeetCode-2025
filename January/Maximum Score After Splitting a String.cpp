df1da9cbf6a9bf19d04fac5145b46dff4080d508
VijayKYadav <116385235+VijayKYadav@users.noreply.github.com>
2025-01-01 12:46:55 +0530

    Add files via upload

---
+++ January/Maximum Score After Splitting a String.cpp
@@ -0,0 +1,55 @@
+I-Approach
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.length();
+        int zCount=0;
+        int oCount=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='1')
+                oCount++;
+        }
+        int score=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]=='0')
+                zCount++;
+            else
+                oCount--;
+            
+            score=max(score,zCount+oCount);
+        }
+
+        return score;
+    }
+};
+
+II-Approach
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.length();
+        int zCount=0;
+        int oCount=0;
+        
+        int score=INT_MIN;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]=='0')
+                zCount++;
+            else
+                oCount++;
+            
+            score=max(score,zCount-oCount);
+        }
+        if(s[n-1]=='1')
+            oCount++;
+
+        return score+oCount;
+    }
+};
