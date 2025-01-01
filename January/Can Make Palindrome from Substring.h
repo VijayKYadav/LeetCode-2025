fd0ebe3e9e82ac035f17df4863e7a2078aa1fd12
VijayKYadav <116385235+VijayKYadav@users.noreply.github.com>
2025-01-01 15:12:54 +0530

    Add files via upload

---
+++ January/Can Make Palindrome from Substring.h
@@ -0,0 +1,73 @@
+I-Approach
+
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        int m=queries.size();
+        vector<bool> ans;
+        for(int i=0;i<m;i++)
+        {
+            int st=queries[i][0];
+            int len=queries[i][1]-queries[i][0]+1;
+            int k=queries[i][2];
+
+            ans.push_back(solve(s.substr(st,len),k));
+        }
+        return ans;
+    }
+
+    bool solve(string s, int k)
+    {
+        int n=s.length();
+        vector<int> mp(26,0);
+
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]-'a']++;
+        }
+        int count=0;
+        for(int i=0;i<26;i++)
+        {
+            count+=mp[i]%2;
+        }
+        
+        if(count/2<=k)
+            return true;
+        else
+            return false;
+    }
+};
+
+II-Approach
+
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        int n=s.length();
+        int m=queries.size();
+        vector<bool> ans;
+
+        vector<vector<int>> dp(n+1,vector<int>(26,00));
+
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=dp[i-1];
+            dp[i][s[i-1]-'a']++;
+        }
+
+        int k,count;
+
+        for(int i=0;i<m;i++)
+        {
+            count=0;
+            for(int j=0;j<26;j++)
+            {
+                k=dp[queries[i][1]+1][j]-dp[queries[i][0]][j];
+                count+=k%2;
+            }
+
+            ans.push_back(count/2<=queries[i][2]?true:false);
+        }
+        return ans;
+    }
+};
