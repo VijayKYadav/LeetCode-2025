b23c6927e60e8407cbc0cbabc2bf811cb248d04a
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-01-20 09:17:01 +0530

    Add files via upload

---
+++ January/First Completely Painted Row or Column.h
@@ -0,0 +1,47 @@
+I Approach
+
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+        unordered_map<int,pair<int,int>> mp;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                mp[mat[i][j]]={i,j};
+            }
+        }
+
+        int ans;
+
+        for(int i=0;i<n*m;i++)
+        {
+            auto p=mp[arr[i]];
+            int r=p.first;
+            int c=p.second;
+
+            row[r]++;
+            col[c]++;
+
+            if(row[r]==m || col[c]==n)
+            {
+                ans=i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
+
+Time Complexity: O(n*m)
+Space Complexity: O(n*m)
+ 
+where, 'n' and 'm' are the number of row's and 
+column's in the given 2-D array 'mat'.
