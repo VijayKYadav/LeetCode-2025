99fae56ee95e975e2ce66cb045e4d49242f78eff
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-01-23 08:53:32 +0530

    Update Sum of Root To Leaf Binary Numbers.h

--- January/Sum of Root To Leaf Binary Numbers.h
+++ January/Sum of Root To Leaf Binary Numbers.h
@@ -25,55 +25,7 @@ public:
     }
 };
 
-
-Time Complexity: O(n*m)
-Space Complexity: O(n*m)
- 
-where, 'n' is the number of row's and 
-'m' is the number of column's in the given square matrix 'grid'.
-
-II Approach
-
-class Solution {
-public:
-    int countServers(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
-        vector<int> row(n,0);
-        vector<int> col(m,0);
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j])
-                {
-                    row[i]++;
-                    col[j]++;
-                }
-            }
-        }
-
-        int count=0;
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j])
-                {
-                    if(row[i]>=2 || col[j]>=2)
-                        count++;
-                }
-            }
-        }
-
-        return count;
-    }
-};
-
 Time Complexity: O(n)
-Space Complexity: O(log(Height of given tree))
+Space Complexity: O(n)
  
 where, 'n' is the number of node's in a given tree'root'.
