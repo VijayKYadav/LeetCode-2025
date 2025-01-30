c4b7824f36c573760e7a28246fc536723b6bd7f1
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-01-30 12:03:07 +0530

    Update Reorder Routes to Make All Paths Lead to the City Zero.cpp

--- January/Reorder Routes to Make All Paths Lead to the City Zero.cpp
+++ January/Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -38,3 +38,58 @@ Time Complexity: O(n)
 Space Complexity: O(n)
  
 where, 'n' is the number of vertice's in a given graph 'connections'.
+
+II Approach (Using BFS)
+
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<pair<int,int>> adj[n];
+        int e=connections.size();
+
+        for(int i=0;i<e;i++)
+        {
+            int u=connections[i][0];
+            int v=connections[i][1];
+
+            adj[u].push_back({v,1});
+            adj[v].push_back({u,0});
+        }
+        vector<int> vi(n);
+        return solve(0,-1,adj,vi);
+    }
+
+    int solve(int node, int pre, vector<pair<int,int>> adj[], vector<int> &vi)
+    {
+        queue<int> q;
+        q.push(node);
+        vi[node]=true;
+        int ans=0;
+        
+        while(!q.empty())
+        {
+            int n=q.front();
+            q.pop();
+
+            for(int i=0;i<adj[n].size();i++)
+            {
+                int v=adj[n][i].first;
+                int val=adj[n][i].second;
+
+                if(!vi[v])
+                {
+                    vi[v]=true;
+                    ans+=val;
+                    q.push(v);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+ 
+where, 'n' is the number of vertice's in a given graph 'connections'.
