f0470b94a4334f05f31acedc8f2438713e1bf6b5
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-03-11 09:50:14 +0530

    Update Count Vowel Substrings of a String.cpp

--- March/Count Vowel Substrings of a String.cpp
+++ March/Count Vowel Substrings of a String.cpp
@@ -47,3 +47,58 @@ Time Complexity=O(n*n).
 Space Complexity=O(n).
 
 Where, 'n' is the length of the given string 'word'.
+    
+
+II Approach (Using Sliding-Window + Unordered_Map) (Optimal Method)
+
+class Solution {
+public:
+    int countVowelSubstrings(string word) {
+        int n=word.length();
+        int count=0;
+        
+        int i=0;
+        int j=0;
+        unordered_map<char,int> mp;
+
+        while(j<n)
+        {
+            if(word[j]=='a' || word[j]=='e' || word[j]=='i' || word[j]=='o' || word[j]=='u')
+                mp[word[j]]++;
+            else
+            {
+                i=j+1;
+                mp.clear();
+            }
+            
+            if(mp.size()==5)
+            {
+                int t=j;
+
+                while(word[t]=='a' || word[t]=='e' || word[t]=='i' || word[t]=='o' || word[t]=='u')
+                    t++;
+
+                while(mp.size()==5)
+                {
+                    count+=t-j;
+                    mp[word[i]]--;
+                    
+                    if(mp[word[i]]==0)
+                        mp.erase(word[i]);
+
+                    i++;
+                }
+            }
+
+            j++;
+        }
+        
+
+        return count;
+    }
+};
+
+Time Complexity=O(n).
+Space Complexity=O(1).
+
+Where, 'n' is the length of the given string 'word'.
