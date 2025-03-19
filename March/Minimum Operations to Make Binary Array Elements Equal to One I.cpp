c986cfb48b708cf6bdab1133cef57a1a3991b282
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-03-19 08:50:55 +0530

    Update Minimum Operations to Make Binary Array Elements Equal to One I.cpp

--- March/Minimum Operations to Make Binary Array Elements Equal to One I.cpp
+++ March/Minimum Operations to Make Binary Array Elements Equal to One I.cpp
@@ -26,38 +26,7 @@ public:
     }
 };
 
-Time Complexity=O(n*n).
-Space Complexity=O(n*n).
-
-Where, 'n' is the size of in a given vector/array 'arr'.
-
-This, solution will give TLE(Time Limit Exceeded).
-
-II Approach (Using Unoreded_Set+Bitwies Operation)(Optimal Method)
-
-class Solution {
-public:
-    int subarrayBitwiseORs(vector<int>& arr) {
-        int n=arr.size();
-
-        unordered_set<int> st;
-
-        for( int i=0;i<n;i++) 
-        {
-            st.insert(arr[i]);
-            for( int j=i-1;j>=0;j--)
-            {
-                if((arr[i]|arr[j])==arr[j])
-                    break;
-                arr[j] |= arr[i];
-                st.insert(arr[j]);
-            }
-        }
-        return st.size();
-    }
-};
-
-Time Complexity=O(n).
+Time Complexity=O(n*3)~O(n).
 Space Complexity=O(1).
 
 Where, 'n' is the size of a given vector/array 'nums'.
