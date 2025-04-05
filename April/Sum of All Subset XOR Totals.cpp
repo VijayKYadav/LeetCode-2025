47c2bf871a23fec30da4301f27f54597e5bfb950
Vijay Kumar Yadav <116385235+VijayKYadav@users.noreply.github.com>
2025-04-05 17:41:28 +0530

    Add files via upload

---
+++ April/Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,52 @@
+I Approach (Using Recursion) (Brute-Force Method)
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n=nums.size();
+        int xorT=0;
+        int sum=0;
+        solve(0,n,sum,xorT,nums);
+        return xorT;
+    }
+
+    void solve(int ind,int n, int sum, int &xorT ,vector<int> &nums)
+    {
+        if(ind==n)
+        {
+            xorT+=sum;
+            return ;
+        }
+
+        solve(ind+1,n,sum^nums[ind],xorT,nums);
+        solve(ind+1,n,sum,xorT,nums);
+    }
+};
+
+Time Complexity=O(2^n).
+Space Complexity=O(n).
+
+Where,'n' is the number of node's in a given tree 'root'.
+
+
+II Approach (Using Bit-Manipulation) (Optimal Method)
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n=nums.size();
+        int xorT=0;
+
+        for(int ele:nums)
+        {
+            xorT|=ele;
+        }
+
+        return xorT<<(n-1);   
+    }
+};
+
+Time Complexity=O(2^n).
+Space Complexity=O(n).
+
+Where,'n' is the number of node's in a given tree 'root'.
